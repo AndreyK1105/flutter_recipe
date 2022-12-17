5700dc07b4826db0fb7f0d4f88ba7ad9818b4ce5
AndreyK1105 <k310031@yandex.ru>
2022-12-17 18:23:18 +1000

    rename

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Otus recipe',
+      title: 'My recipe',
       theme: ThemeData(
         // This is the theme of your application.
         //
